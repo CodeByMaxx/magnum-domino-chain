ab3bfe9dcb5892c3c5b0ad15285313dd117c64d1
Markus <Markus.Gruber4@gmx.net>
2026-06-05 17:49:04 +0200

    .

---
+++ build/CMakeFiles/domino.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/domino.dir/src/main.cpp.o"
+  "CMakeFiles/domino.dir/src/main.cpp.o.d"
+  "domino"
+  "domino.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/domino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
